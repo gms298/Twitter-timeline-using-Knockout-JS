bf65a692bba324331ef84f24f7bac9a735cf123d
Manoj Sharan <gms.94@icloud.com>
2017-05-07 14:35:22 -0400

    Updating links

--- knockout.js
+++ knockout.js
@@ -1,4 +1,4 @@
-window.onload=function() {
+      window.onload=function() {
       var Tweet = function(name, username, time, tweet, retweets, favs, profPic, tweetPic) {
           this.profilePic= profPic;
           this.name = name;
@@ -11,10 +11,10 @@ window.onload=function() {
           }
       var twitterViewModel = {
           tweets: [
-          new Tweet("Taylor", "@tay12", "1m", "Look at this cool way to connect outlets to my ViewController.", "56", "25", "https://media.github.ncsu.edu/user/5816/files/578f138e-8e60-11e6-9a96-b474e3cc9776", "https://media.github.ncsu.edu/user/5816/files/406a0ca6-8e5e-11e6-81ea-826758e9bcd0"),
-          new Tweet("John", "@john47", "3m", "Xcode now shows a live preview of exactly how the app will appear on any Apple device with full vibrancy. Wow..", "3", "6", "https://media.github.ncsu.edu/user/5816/files/707e374e-8e60-11e6-8173-3af17b36ea11", ""),
-          new Tweet("Bertrand", "@bert", "4m", "Xcode 8 just launched. It's a radically faster version which features new editor extensions that can completely customize coding experiences.", "347", "84", "https://media.github.ncsu.edu/user/5816/files/7fdf88fa-8e60-11e6-8a45-cd0352a5194f", "https://media.github.ncsu.edu/user/5816/files/0be304b4-8e5f-11e6-9858-2ff0b56d3c13"),
-          new Tweet("Charles", "@charlie", "5m", "This changes everything. Xcode 8 is way faster and cooler!", "84", "39", "https://media.github.ncsu.edu/user/5816/files/90daa354-8e62-11e6-9dfa-835397035f37", "")
+          new Tweet("Taylor", "@tay12", "1m", "Look at this cool way to connect outlets to my ViewController.", "56", "25", "https://cloud.githubusercontent.com/assets/21252571/25783901/f1cc353c-3331-11e7-93d7-21b9c951174b.png", "https://cloud.githubusercontent.com/assets/21252571/25783905/030a7ad4-3332-11e7-8643-96aa8d5fab23.png"),
+          new Tweet("John", "@john47", "3m", "Xcode now shows a live preview of exactly how the app will appear on any Apple device with full vibrancy. Wow..", "3", "6", "https://cloud.githubusercontent.com/assets/21252571/25783908/111a2930-3332-11e7-85e0-9e6edefa1182.png", ""),
+          new Tweet("Bertrand", "@bert", "4m", "Xcode 8 just launched. It's a radically faster version which features new editor extensions that can completely customize coding experiences.", "347", "84", "https://cloud.githubusercontent.com/assets/21252571/25783911/21ff72d2-3332-11e7-834e-faef0b68d057.png", "https://cloud.githubusercontent.com/assets/21252571/25783914/2be9a588-3332-11e7-997f-36e242f93151.jpeg"),
+          new Tweet("Charles", "@charlie", "5m", "This changes everything. Xcode 8 is way faster and cooler!", "84", "39", "https://cloud.githubusercontent.com/assets/21252571/25783919/373acf34-3332-11e7-8b20-eb3f18ea2699.jpeg", "")
           ]
       };
 ko.applyBindings(twitterViewModel);
